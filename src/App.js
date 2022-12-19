75bc4db3203e8aa3e7bdcab253013c4318ac2c5c
Marcin Lyczko <marcin.lyczko@grandparade.co.uk>
2022-12-19 12:36:58 +0100

    Revert "add charts"
    This reverts commit 74ff3fa94e7b9596ccd352c84d085e4192badebb.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { slide as Menu } from 'react-burger-menu'
-import React from "react";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
-import Home from "./home/home";
-import DefaultChart from "./default-chart/default-chart";
-import PanZoomChart from "./pan-zoom-chart/pan-zoom-chart";
-import CustomNodeChart from "./custom-node-chart/custom-node-chart";
-import ExportChart from "./export-chart/export-chart";
-import DragDropChart from "./drag-drop-chart/drag-drop-chart";
-import EditChart from "./edit-chart/edit-chart";
-import EditNode from "./edit-node/edit-node";
 
 function App() {
   return (
-    <Router>
-      <div className="wrapper">
-        <div className="App">
-          <Menu>
-            <a id="my-charts" className="menu-item" href="#">My charts</a>
-            <a id="shared-with-me" className="menu-item" href="#">Shared with me</a>
-            <a id="starred" className="menu-item" href="#">Stared</a>
-            <a id="recent" className="menu-item" href="#">Recent</a>
-            <a id="offline" className="menu-item" href="#">Offline</a>
-            <a id="uploads" className="menu-item" href="#">Uploads</a>
-          </Menu>
-          <nav>
-            <NavLink to="/" exact activeClassName="selected">
-              Home
-            </NavLink>
-            <NavLink to="/default-chart" activeClassName="selected">
-              Deault Chart
-            </NavLink>
-            <NavLink to="/pan-zoom-chart" activeClassName="selected">
-              Pan & Zoom Chart
-            </NavLink>
-            <NavLink to="/custom-node-chart" activeClassName="selected">
-              Custom Node Chart
-            </NavLink>
-            <NavLink to="/export-chart" activeClassName="selected">
-              Export Chart
-            </NavLink>
-            <NavLink to="/drag-drop-chart" activeClassName="selected">
-              Drap & Drop Chart
-            </NavLink>
-            <NavLink to="/edit-chart" activeClassName="selected">
-              Edit Chart
-            </NavLink>
-            <NavLink to="/edit-node" activeClassName="selected">
-              Edit Node
-            </NavLink>
-          </nav>
-
-          <Route exact path="/" component={Home} />
-          <Route path="/default-chart" component={DefaultChart} />
-          <Route path="/pan-zoom-chart" component={PanZoomChart} />
-          <Route path="/custom-node-chart" component={CustomNodeChart} />
-          <Route path="/export-chart" component={ExportChart} />
-          <Route path="/drag-drop-chart" component={DragDropChart} />
-          <Route path="/edit-chart" component={EditChart} />
-          <Route path="/edit-node" component={EditNode} />
-        </div>
-
-      </div>
-
-    </Router>
+    <div className="App">
+      <Menu>
+        <a id="my-charts" className="menu-item" href="#">My charts</a>
+        <a id="shared-with-me" className="menu-item" href="#">Shared with me</a>
+        <a id="starred" className="menu-item" href="#">Stared</a>
+        <a id="recent" className="menu-item" href="#">Recent</a>
+        <a id="offline" className="menu-item" href="#">Offline</a>
+        <a id="uploads" className="menu-item" href="#">Uploads</a>
+      </Menu>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
