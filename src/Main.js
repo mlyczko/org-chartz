fdc4198808deb45ca919e85e22d743a2f4950787
Marcin Lyczko <marcin.lyczko@grandparade.co.uk>
2022-12-20 11:32:52 +0100

    split code

---
+++ src/Main.js
@@ -0,0 +1,80 @@
+const Main = () => {
+    return (
+        <main>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col mb-4"></div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Grzegorz Nitro</div>
+                            <div>CEO</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4"></div>
+                </div>
+                <div className="row">
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                    <div className="col mb-4">
+                        <div className="bg-secondary bg-gradient text-white p-3">
+                            <div>Jan Kowalski</div>
+                            <div>Account Executive</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main;
