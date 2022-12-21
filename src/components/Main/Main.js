92e4391eb57db376598b36d47aedb3d60d2d4ad6
Daniel Mielnik <daniel.mielnik.ctr@sabre.com>
2022-12-21 14:16:28 +0100

    mock for nodes added, elements renders conditionally

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,80 +1,70 @@
 import Depth from './components/Depth';
 
-const Main = () => {
+const getParentList = (nodes) => {
+    const parentList = [];
+    let focussed = nodes.parent;
+    do {
+        focussed && Object.keys(focussed).length && parentList.push(focussed);
+        focussed = focussed?.parent;
+    } while (!focussed || Object.keys(focussed).length)
+    return parentList.reverse();
+}
+
+const getChildrenList = (nodes) => {
+    const childrenList = [];
+    let focussed = nodes.children
+    do {
+        focussed?.forEach(element => {
+            element && Object.keys(element).length && childrenList.push(element);
+            focussed = element?.children;
+        })
+    } while (focussed && Object.keys(focussed).length)
+    return childrenList
+}
+
+const renderParents = (nodes) => {
+    const parentList = getParentList(nodes);
+    return parentList.map(parent => (
+        <div className={"row"}>
+            <div className="col mb-4">
+                <div className="bg-secondary bg-gradient text-white p-3">
+                    <div>{`${parent.name} ${parent.lastname}`}</div>
+                    <div>{parent.role}</div>
+                </div>
+            </div>
+        </div>
+    ))
+}
+
+const renderChildren = (nodes) => {
+    const childrenList = getChildrenList(nodes);
+    return childrenList.map(children => (
+        <div className="col mb-4">
+            <div className="bg-secondary bg-gradient text-white p-3">
+                <div>{`${children.name} ${children.lastname}`}</div>
+                <div>{children.role}</div>
+            </div>
+        </div>
+    ))
+}
+
+const Main = ({nodes}) => {
     return (
         <main>
             <div className="container text-center">
                 <div className="d-flex">
                     <div className="flex-grow-1 me-3 mt-5">
-                        <div className="row">
-                            <div className="col mb-4"></div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Grzegorz Nitro</div>
-                                    <div>CEO</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4"></div>
-                        </div>
+                        {renderParents(nodes)}
                         <div className="row">
                             <div className="col mb-4">
                                 <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
+                                    <div>{`${nodes.name} ${nodes.lastname}`}</div>
+                                    <div>{nodes.role}</div>
                                 </div>
                             </div>
                         </div>
                         <div className="row">
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
-                            <div className="col mb-4">
-                                <div className="bg-secondary bg-gradient text-white p-3">
-                                    <div>Jan Kowalski</div>
-                                    <div>Account Executive</div>
-                                </div>
-                            </div>
+                            {renderChildren(nodes)}
                         </div>
                     </div>
                     <div>
